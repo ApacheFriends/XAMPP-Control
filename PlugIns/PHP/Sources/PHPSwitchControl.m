291627a548d800c41624e576e7a6136c1d92d1b5
Christian Speich <kleinweby@kleinweby.de>
2009-05-27 21:32:18 +0200

    Adding Copyright infos

--- PlugIns/PHP/Sources/PHPSwitchControl.m
+++ PlugIns/PHP/Sources/PHPSwitchControl.m
@@ -1,10 +1,27 @@
-//
-//  PHPSwitchControl.m
-//  XAMPP Control
-//
-//  Created by Christian Speich on 16.05.09.
-//  Copyright 2009 __MyCompanyName__. All rights reserved.
-//
+/*
+ 
+ XAMPP
+ Copyright (C) 2009 by Apache Friends
+ 
+ Authors of this file:
+ - Christian Speich <kleinweby@apachefriends>
+ 
+ This file is part of XAMPP.
+ 
+ XAMPP is free software: you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as published by
+ the Free Software Foundation, either version 3 of the License, or
+ (at your option) any later version.
+ 
+ XAMPP is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+ 
+ You should have received a copy of the GNU General Public License
+ along with XAMPP.  If not, see <http://www.gnu.org/licenses/>.
+ 
+ */
 
 #import "PHPSwitchControl.h"
 
--- Sources/GettingStartedController.h
+++ Sources/GettingStartedController.h
@@ -1,11 +1,27 @@
-/* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
- * XAMPP Control - Controls XAMPP                                    *
- * Copyright (C) 2007-2009  Christian Speich <kleinweby@kleinweby.de>*
- *                                                                   *
- * XAMPP Control comes with ABSOLUTELY NO WARRANTY; This is free     * 
- * software, and you are welcome to redistribute it under certain    *
- * conditions; see COPYING for details.                              *
- * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
+/*
+ 
+ XAMPP
+ Copyright (C) 2009 by Apache Friends
+ 
+ Authors of this file:
+ - Christian Speich <kleinweby@apachefriends>
+ 
+ This file is part of XAMPP.
+ 
+ XAMPP is free software: you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as published by
+ the Free Software Foundation, either version 3 of the License, or
+ (at your option) any later version.
+ 
+ XAMPP is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+ 
+ You should have received a copy of the GNU General Public License
+ along with XAMPP.  If not, see <http://www.gnu.org/licenses/>.
+ 
+ */
 
 #import <Cocoa/Cocoa.h>
 #import <WebKit/WebKit.h>
